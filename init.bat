f1ead601531706862dab279752ce4fd2ddf6d2de
Velsych <velsych@mail.ru>
2026-08-21 18:33:23 +0300

    init fix 4 screw exe

--- init.bat
+++ init.bat
@@ -2,9 +2,10 @@
 uv sync
 uv build
 uv tool install dist/meta_shit-0.1.0-py3-none-any.whl
-echo MODEL_NAME= > .env
-echo API_LINK= >> .env
-uv run pyinstaller --distpath "%cd%" --onefile images_parser/scripts/app.py
+IF NOT EXIST %cd%\.env (
+    echo MODEL_NAME= > .env
+    echo API_LINK= >> .env
+)
 setenv %cd%\.env
 md %cd%\images_parser\images
 echo Exe file created
